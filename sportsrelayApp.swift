e22b539cedd429ca03e9f80e76a70d95816a403d
napolaeOng <baybayhoo@gmail.com>
2025-07-02 17:34:01 +0900

    test2
    test2

---
+++ sportsrelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportsrelayApp.swift
+//  sportsrelay
+//
+//  Created by 권소정 on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct sportsrelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //ContentView()
+        }
+    }
+}
